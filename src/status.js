5a0a340a8c4d37d9bc638eaa86c2174c91776f8f
unorji kingsley <unorjikingsley@gmail.com>
2023-05-05 08:02:28 +0100

    Fixed bugs in my code

--- src/status.js
+++ src/status.js
@@ -1,14 +1,5 @@
-// const tasks = JSON.parse(localStorage.getItem('tasks'));
-
-// function completeStatus(index, completed) {
-//   tasks[index].completed = completed;
-//   localStorage.setItem('tasks', JSON.stringify(tasks));
-// }
-
-// export default completeStatus;
-
-const storedTasks = localStorage.getItem('task');
-const tasks = storedTasks ? JSON.parse(storedTasks) : [];
+// eslint-disable-next-line prefer-const
+let tasks = JSON.parse(localStorage.getItem('tasks') || []);
 
 function CompleteStatus(index) {
   tasks[index].completed = true;
