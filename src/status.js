ccfac05fd2939aaabcca312d382c50a083926191
unorji kingsley <unorjikingsley@gmail.com>
2023-05-05 22:46:51 +0100

    Finally corrected all errors

--- src/status.js
+++ src/status.js
@@ -1,8 +1,8 @@
-let tasks = JSON.parse(localStorage.getItem('tasks') || []);
-if (localStorage.getItem('tasks')) {
-  tasks = JSON.parse(localStorage.getItem('tasks'));
-}
 function CompleteStatus(index) {
+  let tasks = JSON.parse(localStorage.getItem('tasks') || []);
+  if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
   if (tasks[index]) {
     tasks[index].completed = true;
     localStorage.setItem('tasks', JSON.stringify(tasks));
